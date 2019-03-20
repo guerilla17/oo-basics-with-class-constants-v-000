e376a1192038bd3af804ac0c2af571951654003a
Gigi M <mmgamao1@gmail.com>
2019-03-20 18:15:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,14 +7,13 @@ class Book
   def initialize(title)
     @title = title
   end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-  
   def genre=(genre)
     @genre = genre
     GENRES << GENRE  
-  end  
+  end 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
 
 end
